2c85ed93adb6974b4b1a28eec73d527722f5cb4e
lichong0564 <lichong0564@163.com>
2018-02-20 23:43:07 -0600

    add asf namezxzxz

--- src/cnic/uiuc/lichong/helloworld2.java
+++ src/cnic/uiuc/lichong/helloworld2.java
@@ -5,6 +5,6 @@ public class helloworld2 {
 
     public static void main(String[] args){
 
-      System.out.print("hello world 李ghaag\n");
+      System.out.print("hello world 李ghaagasd\n");
     }
 }
